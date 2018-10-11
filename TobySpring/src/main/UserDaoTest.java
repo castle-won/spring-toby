2b3f9dd3f48761887886bd080df132e8527405bd
magic0102 <ysw0111@naver.com>
2018-10-12 01:29:42 +0900

    first commit

---
+++ TobySpring/src/main/UserDaoTest.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("user");
+		user.setName("윤성원");
+		user.setPassword("sungwon");
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
